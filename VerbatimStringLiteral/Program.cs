3b71b1d58d22f756cffc07c21ea7d6dc5bf83645
CARICOM12 <0190a@ukr.net>
2020-05-02 20:11:57 +0300

    Add files via upload

---
+++ VerbatimStringLiteral/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VerbatimStringLiteral
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(@" This is a code 
+                                 produced by a noob ""developer"" 
+                                 who is struggling
+             to make a 
+             living");
+        }
+    }
+}
